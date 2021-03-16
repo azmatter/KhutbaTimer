7ea44898fd58824811745f8ea761571d441de604
Azeem <azeemans@gmail.com>
2021-03-16 15:16:02 -0400

    Handling change on IAR website to how the Jumuah times are formatted

---
+++ app/src/main/java/com/iar/mtohmaz/khutbatimer/KhateebTimesHelper.java
@@ -0,0 +1,188 @@
+package com.iar.mtohmaz.khutbatimer;
+
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.util.Log;
+import android.app.Activity;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class KhateebTimesHelper {
+
+    String TAG = "KhutbaTimer";
+    Activity MainActivity;
+
+    public static final int TIME30 = 1800000,   // 30 minutes (in ms)
+            TIME20 = 1200000,   // 20 minutes
+            TIME15=900000,      // 15 minutes
+            TIME10=600000,      // 10 minutes
+            TIME05=305000,      // 5 minutes
+            TIME01=60000,       // 1 minute
+            TIME5sec=5000;      // 5 seconds
+    private int TIMER_COUNTDOWN_LENGTH;
+    private String lastSyncTime = "";
+    public final String MASJID_KHUTBA_TIMES_URL = "http://www.raleighmasjid.org/m";
+
+
+    public KhateebTimesHelper (Activity context){
+        MainActivity = context;
+    }
+
+    /**
+     * Main method for parsing times from web.
+     * Assuming this is the time format being retrieved from the website:  "Khutba 11:00 AM"
+     *
+     * @return string array of times, each formatted as "HH:MM"
+     */
+    public String[] parseKhutbaTimesFromWeb() {
+
+        TAG = "parseKhutbaTimesFromWeb";
+        String shiftTimes[] = null;
+        boolean parserError = false;
+
+        try {
+            Document doc = Jsoup.connect(MASJID_KHUTBA_TIMES_URL).get();
+            Elements list = doc.getElementsByClass("time");
+
+            shiftTimes = new String[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                // see javadoc above for expected time formatting. If the website changes the expected format, this will fail.
+                String token = StringUtils.right(list.get(i).text(), 8).trim();
+                String temp[] = token.split(":|\\s+", 3);
+
+                String  tempHour = "" + temp[0],
+                        tempMin = "" + temp[1];
+                Log.d(TAG, "Shift " + (i+1) + ": " + tempHour + tempMin);
+                shiftTimes[i] = convertTo24Hour(Integer.parseInt(tempHour)) + ":" + tempMin;
+                if(shiftTimes[i].equals(null)){
+                    parserError = true;
+                    break;
+                }
+            }
+            Log.i(TAG, "Retrieved " + shiftTimes.length + " shifts from website " + Arrays.toString(shiftTimes));
+            if(parserError){
+                Log.e(TAG,"Parsing failed, check times format on website");
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG,e.toString());
+        }
+
+        return shiftTimes;
+    }
+
+    public int convertTo24Hour(int time) {
+        if (time < 10) {
+            return time + 12;
+        }
+        return time;
+    }
+
+    public String convertTo12Hour(String time) {
+        String ret = "";
+        try {
+            String _24HourTime = time;
+            SimpleDateFormat _24HourSDF = new SimpleDateFormat("HH:mm");
+            SimpleDateFormat _12HourSDF = new SimpleDateFormat("hh:mm a");
+            Date _24HourDt = _24HourSDF.parse(_24HourTime);
+            ret = _12HourSDF.format(_24HourDt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    public String getLastSyncTime(){
+        TAG = "getLastSyncTime";
+        SharedPreferences sharedPreferences = MainActivity.getPreferences(Context.MODE_PRIVATE);
+        String ret = sharedPreferences.getString("lastSyncTime","");
+        Log.i(TAG,"stored lastSyncTime="+ret);
+        return ret;
+    }
+
+    // This is where the default timer countdown length will be initialized.
+    public int getTIMER_COUNTDOWN_LENGTH(){
+        TAG = "getTimerLength";
+        SharedPreferences sharedPreferences = MainActivity.getPreferences(Context.MODE_PRIVATE);
+        int ret = sharedPreferences.getInt("countdownLength",TIME20);
+        Log.i(TAG,"stored countdownLength: " + ret);
+        return ret;
+    }
+
+    public void setNewTIMER_COUNTDOWN_LENGTH(int newMins){
+        TAG = "setNewTimerLength";
+        int newTime = newMins*60000;
+        Log.i(TAG, "oldLength=" + this.TIMER_COUNTDOWN_LENGTH/60000 + ", newLength="+newMins);
+
+        // update instance variable
+        this.TIMER_COUNTDOWN_LENGTH = newTime;
+
+        // update shared prefs
+        SharedPreferences sharedPreferences = MainActivity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor sp = sharedPreferences.edit();
+        sp.putInt("countdownLength",newTime);
+        sp.apply();
+    }
+
+    public void storeTimesLocally(String [] times, String lastSyncTime){
+        TAG = "storeTimesLocally";
+        SharedPreferences sharedPreferences = MainActivity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor sp = sharedPreferences.edit();
+        StringBuilder sb = new StringBuilder();
+
+        // Store the # shifts
+        sp.putInt("numShifts",times.length);
+
+        // store the shift times as one string
+        for(int i =0;i<times.length;i++){
+            sb.append(times[i]).append(",");
+        }
+        sp.putString("shiftTimes",sb.toString());
+        Log.d(TAG,"putString: "+ sb.toString());
+
+        // store the lastSyncTime
+        sp.putString("lastSyncTime",lastSyncTime);
+
+        boolean tf = sp.commit();
+        if(tf)
+            Log.i(TAG,"Successfull wrote times to sharedPreferences");
+        else Log.e(TAG, "Error while writing times to sharedPreferences");
+
+    }
+
+    /*
+        Provides stored times as backup in case getKhutbaTimesFromWeb() is unable to read the masjid website.
+     */
+    public String[] getStoredTimes() {
+        TAG = "getStoredTimes";
+        String [] goodTimes;
+        int numShifts = 0;
+        String str="";
+
+        try {
+            SharedPreferences sharedPreferences = MainActivity.getPreferences(Context.MODE_PRIVATE);
+            numShifts = sharedPreferences.getInt("numShifts", 0);
+            str = sharedPreferences.getString("shiftTimes", null);
+        }
+        catch (Exception e)
+        {
+            Log.e(TAG,e.toString());
+        }
+        Log.i(TAG,"Received numShifts: " + numShifts +"\nstoredTimes: " + str);
+
+        if(numShifts==0 || str ==null) {
+            Log.e(TAG,"Nothing stored in sharedPreferences");
+            return null;
+        }
+        goodTimes = str.split(",");
+        Log.i(TAG,"Parsed times: " + Arrays.toString(goodTimes));
+        return goodTimes;
+    }
+
+}
--- app/src/main/java/com/iar/mtohmaz/khutbatimer/MainActivity.java
+++ app/src/main/java/com/iar/mtohmaz/khutbatimer/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.os.Bundle;
@@ -23,30 +22,25 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.apache.commons.lang3.StringUtils;
 
-public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickListener{
+public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickListener {
 
     private static final String FORMAT = "%02d:%02d";
     private String shift = "";
     private String nextShift = "";
     private String statusMsg = "";
-    private String lastSyncTime = "";
     TextView text1;
     TextView text2;
     TextView text3;
     Button menuButton;
     PopupMenu popupMenu;
     CountDownTimer timer = null;
-    String shiftTimes[];
-    String TAG = "KhutbaTimer";
 
-    static final int TIME30 = 1800000, TIME20 = 1200000, TIME15=900000, TIME10=600000, TIME05=305000, TIME01=60000, TIME5sec=5000;
-    int TIMER_COUNTDOWN_LENGTH;
-    final String MASJID_KHUTBA_TIMES_URL = "http://www.raleighmasjid.org/m";
+    String TAG = "KhutbaTimer";
+    String shiftTimes[];
+    KhateebTimesHelper KTHelper;
+    private int TIMER_COUNTDOWN_LENGTH;
+    private String lastSyncTime = "";
     final boolean testing = false;
 
     @Override
@@ -81,10 +75,10 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
 
         setDisplay("","Khateeb Timer");
 
-        TIMER_COUNTDOWN_LENGTH = getTIMER_COUNTDOWN_LENGTH();
-
-        lastSyncTime = getLastSyncTime();
-
+        //Initialize instance variables
+        KTHelper = new KhateebTimesHelper(this);
+        TIMER_COUNTDOWN_LENGTH = KTHelper.getTIMER_COUNTDOWN_LENGTH();
+        lastSyncTime = KTHelper.getLastSyncTime();
         refreshKhutbahTimes();
 
     }
@@ -92,120 +86,6 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
     private void setDisplay(String txt_timer, String txt_sub) {
         text2.setText(txt_timer);
         text1.setText(txt_sub);
-    }
-
-    public String getLastSyncTime(){
-        TAG = "getLastSyncTime";
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        String ret = sharedPreferences.getString("lastSyncTime","");
-        Log.i(TAG,"stored lastSyncTime="+ret);
-        return ret;
-    }
-
-    // This is where the default timer countdown length will be initialized.
-    public int getTIMER_COUNTDOWN_LENGTH(){
-        TAG = "getTimerLength";
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        int ret = sharedPreferences.getInt("countdownLength",TIME20);
-        Log.i(TAG,"stored countdownLength: " + ret);
-        return ret;
-    }
-
-    public void setNewTIMER_COUNTDOWN_LENGTH(int newMins){
-        TAG = "setNewTimerLength";
-        int newTime = newMins*60000;
-        Log.i(TAG, "oldLength=" + this.TIMER_COUNTDOWN_LENGTH/60000 + ", newLength="+newMins);
-
-        // update instance variable
-        this.TIMER_COUNTDOWN_LENGTH = newTime;
-
-        // update shared prefs
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor sp = sharedPreferences.edit();
-        sp.putInt("countdownLength",newTime);
-        sp.apply();
-    }
-
-    private void storeTimesLocally(String [] times, String lastSyncTime){
-        TAG = "storeTimesLocally";
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor sp = sharedPreferences.edit();
-        StringBuilder sb = new StringBuilder();
-
-        // Store the # shifts
-        sp.putInt("numShifts",times.length);
-
-        // store the shift times as one string
-        for(int i =0;i<times.length;i++){
-            sb.append(times[i]).append(",");
-        }
-        sp.putString("shiftTimes",sb.toString());
-        Log.d(TAG,"putString: "+ sb.toString());
-
-        // store the lastSyncTime
-        sp.putString("lastSyncTime",lastSyncTime);
-
-        boolean tf = sp.commit();
-        if(tf)
-            Log.i(TAG,"Successfull wrote times to sharedPreferences");
-        else Log.e(TAG, "Error while writing times to sharedPreferences");
-
-    }
-
-    /*
-        Provides hardcoded times as backup in case getKhutbaTimesFromWeb() is unable to read the masjid website.
-        Adjust the number of shifts accordingly.
-     */
-    private String[] getStoredTimes() {
-        TAG = "getStoredTimes";
-        String [] goodTimes = {"11:00", "12:00", "13:00", "14:15"};
-        int numShifts = 0;
-        String str="";
-
-        try {
-            SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-            numShifts = sharedPreferences.getInt("numShifts", 0);
-            str = sharedPreferences.getString("shiftTimes", null);
-        }
-        catch (Exception e)
-        {
-            Log.e(TAG,e.toString());
-        }
-        Log.i(TAG,"Received numShifts: " + numShifts +"\nstoredTimes: " + str);
-
-        if(numShifts==0 || str ==null) {
-            Log.e(TAG,"Nothing stored in sharedPreferences");
-            return null;
-        }
-        goodTimes = str.split(",");
-        Log.i(TAG,"Parsed times: " + Arrays.toString(goodTimes));
-        return goodTimes;
-    }
-
-    private String[] getKhutbaTimesFromWeb() {
-
-        TAG = "getKhutbaTimesFromWeb";
-        String shiftTimes[] = null;
-
-        try {
-            Document doc = Jsoup.connect(MASJID_KHUTBA_TIMES_URL).get();
-            Elements list = doc.getElementsByClass("time");
-
-            shiftTimes = new String[list.size()];
-            for (int i = 0; i < list.size(); i++) {
-                String token = StringUtils.right(list.get(i).text(), 5).trim();
-                String temp[] = token.split(":", 2);
-                String  tempHour = "" + temp[0],
-                        tempMin = "" + temp[1];
-                Log.d(TAG, "Shift " + (i+1) + ": " + tempHour + tempMin);
-                shiftTimes[i] = convertTo24Hour(Integer.parseInt(tempHour)) + ":" + tempMin;
-            }
-            Log.i(TAG, "Retrieved " + shiftTimes.length + " shifts from website " + Arrays.toString(shiftTimes));
-
-        } catch (Exception e) {
-            Log.e(TAG,e.toString());
-        }
-        return shiftTimes;
     }
 
     public void refreshKhutbahTimes(){
@@ -216,7 +96,7 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
             public void run() {
 
                 try{
-                    String times[] = getKhutbaTimesFromWeb();
+                    String times[] = KTHelper.parseKhutbaTimesFromWeb();
 
                     if (times != null) {
                         shiftTimes = times;
@@ -224,12 +104,12 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
                         statusMsg = "";
 
                         // Overwrite saved times in sharedPrefs
-                        storeTimesLocally(shiftTimes,lastSyncTime);
+                        KTHelper.storeTimesLocally(shiftTimes,lastSyncTime);
                     }
 
                     else {  // if unable to get times from website, then use stored or hardcoded times
-                        Log.e(TAG, "JSoup unable to retrieve times from " + MASJID_KHUTBA_TIMES_URL);
-                        shiftTimes = getStoredTimes();
+                        Log.e(TAG, "JSoup unable to retrieve times from " + KTHelper.MASJID_KHUTBA_TIMES_URL);
+                        shiftTimes = KTHelper.getStoredTimes();
                         if(shiftTimes!=null) {
                             Log.i(TAG, "Using stored times " + Arrays.toString(shiftTimes));
                             statusMsg = "Unable to reach IAR website. Using times from last sync";
@@ -281,15 +161,15 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
                                 if (shiftTimes[i].equals(currentTime)) {
                                     if (i == 0) {
                                         shift = "First";
-                                        nextShift = "Next shift at " + convertTo12Hour(shiftTimes[1]);
+                                        nextShift = "Next shift at " + KTHelper.convertTo12Hour(shiftTimes[1]);
                                     }
                                     else if (i == 1) {
                                         shift = "Second";
-                                        nextShift = "Next shift at " + convertTo12Hour(shiftTimes[2]);
+                                        nextShift = "Next shift at " + KTHelper.convertTo12Hour(shiftTimes[2]);
                                     }
                                     else if (i == 2 ){
                                         shift = "Third";
-                                        nextShift = "Next shift at " + convertTo12Hour(shiftTimes[3]);
+                                        nextShift = "Next shift at " + KTHelper.convertTo12Hour(shiftTimes[3]);
                                     }
                                     else {
                                         shift = "Fourth";
@@ -323,7 +203,7 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
 
     private void startSalahWarningTimer() {
         Log.d("post-Timer", "TIMER COUNTDOWN EXPIRED!  Displaying Salah! for " + shift + " Shift");
-        CountDownTimer fiveTime = new CountDownTimer(!testing? TIME05:TIME01, 1000) {
+        CountDownTimer fiveTime = new CountDownTimer(!testing? KhateebTimesHelper.TIME05:KhateebTimesHelper.TIME01, 1000) {
             boolean blink = true;
             public void onTick(long millisUntilFinished) {
                 if (blink) {
@@ -351,7 +231,7 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
         if (timer == null) {
             text1.setText(shift + " Shift");
             text3.setText("");
-            timer = new CountDownTimer(!testing? TIMER_COUNTDOWN_LENGTH:TIME01, 1000) {
+            timer = new CountDownTimer(!testing? TIMER_COUNTDOWN_LENGTH: KhateebTimesHelper.TIME01, 1000) {
 
                 boolean blink = true;
                 public void onTick(long millisUntilFinished) {
@@ -396,27 +276,6 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
         }
     }
 
-    private int convertTo24Hour(int time) {
-        if (time < 10) {
-            return time + 12;
-        }
-        return time;
-    }
-
-    private String convertTo12Hour(String time) {
-        String ret = "";
-        try {
-            String _24HourTime = time;
-            SimpleDateFormat _24HourSDF = new SimpleDateFormat("HH:mm");
-            SimpleDateFormat _12HourSDF = new SimpleDateFormat("hh:mm a");
-            Date _24HourDt = _24HourSDF.parse(_24HourTime);
-            ret = _12HourSDF.format(_24HourDt);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ret;
-    }
-
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
         TAG = "Alert Dialog";
@@ -454,7 +313,7 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
             }
             case R.id.timerLength: {
                 dialog.setTitle("Khutba Timer Length");
-                int currTimerLength = MainActivity.this.getTIMER_COUNTDOWN_LENGTH()/60000;
+                int currTimerLength = MainActivity.this.KTHelper.getTIMER_COUNTDOWN_LENGTH()/60000;
                 message = "Current Countdown (mins): "+ (currTimerLength);
 
                 final EditText edittext = new EditText(this);
@@ -473,7 +332,7 @@ public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickL
                         Log.d(TAG,"onClick: Text input: " + input);
                         try {
                             int newtimer = Integer.parseInt(input);
-                            MainActivity.this.setNewTIMER_COUNTDOWN_LENGTH(newtimer);
+                            MainActivity.this.KTHelper.setNewTIMER_COUNTDOWN_LENGTH(newtimer);
                             Toast.makeText(MainActivity.this, "Updating timer countodwn...", Toast.LENGTH_SHORT).show();
                         }
                         catch (NumberFormatException e){
